6428974708b76eecdab838cbcbf59a2081236cdb
JamieSinn <james.sinn@sinndevelopment.com>
2015-03-13 09:52:27 -0400

    Revert "Correct input"
    This reverts commit 9449b0a63aaf6510be13a0b912ccaab06667d9b6.

--- src/Subsystems/PickupSubsystem.cpp
+++ src/Subsystems/PickupSubsystem.cpp
@@ -25,8 +25,28 @@ void PickupSubsystem::Intake(float x, float y, float direction)
 {
 	float speed = sqrt((x*x) + (y*y));
 
-	intakeLeft.Set(speed);
-	intakeRight.Set(-speed);
+	//intakeLeft.Set(speed);
+	//intakeRight.Set(-speed);
+	if(y > 0.25 || y < -0.25){
+		if(x < -0.5f || x > 0.5){
+				intakeLeft.Set(x);
+				intakeRight.Set(x);
+		}
+		else{
+			intakeLeft.Set(y);
+			intakeRight.Set(-y);
+		}
+	}
+	else{
+			intakeLeft.Set(0);
+			intakeRight.Set(0);
+		}
+	/*else
+	{
+		intakeRight.Set(y);
+		intakeLeft.Set(-y);
+	}*/
+
 }
 void PickupSubsystem::Reset()
 {
